dd95f5831a53c498193ba3831cde9e952255b556
Rahul suthar <sutharrahul119@gmail.com>
2025-01-29 17:10:49 +0530

    user model schema create

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(
   })
 );
 
-app.use(express.json);
+app.use(express.json); //can also set a limit app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
---
+++ src/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: true, // cludinary Url
+      required: true,
+    },
+    coverImage: {
+      type: true, // cludinary Url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);

7593eca1516d3031e2c32ebf466b7d3d1974317c
Rahul suthar <sutharrahul119@gmail.com>
2025-02-25 13:32:08 +0530

    mongoDb pipeline for subscription data

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -16,4 +16,4 @@ const subscriptionSchema = new Schema(
   }
 );
 
-export const Subscription = mongoose.model("subscription", subscriptionSchema);
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);

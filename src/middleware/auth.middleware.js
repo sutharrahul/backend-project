f8a09fa467e5edb903a73269aaf19657b2ca126b
Rahul suthar <sutharrahul119@gmail.com>
2025-04-02 16:38:46 +0530

    auth middle ware typo fix

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,31 +1,32 @@
-import { User } from "../models/user.model";
-import { ApiError } from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
     const token =
-      req.cookies?.accessToke ||
-      req.header("Authorization")?.replace("Bearer", "");
-    //
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
     //  req.header("Authorization")?.replace("Bearer", "") use for mobile api bcoz in mobile we dont have cookies
-
+    // console.log(token);
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
+
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     const user = await User.findById(decodedToken?._id).select(
-      "-password -refereshToken"
+      "-password -refreshToken"
     );
 
     if (!user) {
       throw new ApiError(401, "Invalid Access Token");
     }
+
     req.user = user;
     next();
   } catch (error) {
-    throw new ApiError(401, error?.message || "Invalid Access Token");
+    throw new ApiError(401, error?.message || "Invalid access token");
   }
 });

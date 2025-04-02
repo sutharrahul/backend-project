a2b82c7c62d4d7c22f0e160d3335007a2871f2db
Rahul suthar <sutharrahul119@gmail.com>
2025-04-02 16:33:03 +0530

    user model typo fix

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+
 const userSchema = new Schema(
   {
     username: {
       type: String,
       required: true,
       unique: true,
-      lowecase: true,
+      lowercase: true,
       trim: true,
       index: true,
     },
@@ -25,11 +26,11 @@ const userSchema = new Schema(
       index: true,
     },
     avatar: {
-      type: String, // cludinary Url
+      type: String, // cloudinary url
       required: true,
     },
     coverImage: {
-      type: String, // cludinary Url
+      type: String, // cloudinary url
     },
     watchHistory: [
       {
@@ -54,11 +55,12 @@ const userSchema = new Schema(
 //  do this before save
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next(); // to check is password modifyed or no
+
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
-
 // Custom Method to check password
+
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
@@ -68,13 +70,13 @@ userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
-      email: this._email,
+      email: this.email,
       username: this.username,
       fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn: ACCESS_TOKEN_EXPIRY,
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
   );
 };
@@ -87,8 +89,9 @@ userSchema.methods.generateRefreshToken = function () {
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
-      expiresIn: REFRESH_TOKEN_EXPIRY,
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
   );
 };
+
 export const User = mongoose.model("User", userSchema);
